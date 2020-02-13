d6287547a3979fa37d3d3a413686ccc6d267f80e
Alexander Izmailov <yarolig@gmail.com>
2020-02-13 13:18:02 +0300

    refactor OBJ class

--- objloader.py
+++ objloader.py
@@ -3,45 +3,49 @@ import pygame
 from OpenGL.GL import *
 
 
-def MTL(filename):
-    contents = {}
-    mtl = None
-    dirname = os.path.dirname(filename)
-
-    for line in open(filename, "r"):
-        if line.startswith('#'): continue
-        values = line.split()
-        if not values: continue
-        if values[0] == 'newmtl':
-            mtl = contents[values[1]] = {}
-        elif mtl is None:
-            raise ValueError("mtl file doesn't start with newmtl stmt")
-        elif values[0] == 'map_Kd':
-            # load the texture referred to by this declaration
-            mtl[values[0]] = values[1]
-            imagefile = os.path.join(dirname, mtl['map_Kd'])
-            surf = pygame.image.load(imagefile)
-            image = pygame.image.tostring(surf, 'RGBA', 1)
-            ix, iy = surf.get_rect().size
-            texid = mtl['texture_Kd'] = glGenTextures(1)
-            glBindTexture(GL_TEXTURE_2D, texid)
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
-                GL_LINEAR)
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
-                GL_LINEAR)
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, ix, iy, 0, GL_RGBA,
-                GL_UNSIGNED_BYTE, image)
-        else:
-            mtl[values[0]] = list(map(float, values[1:]))
-    return contents
-
 class OBJ:
+    @classmethod
+    def loadTexture(cls, imagefile):
+        surf = pygame.image.load(imagefile)
+        image = pygame.image.tostring(surf, 'RGBA', 1)
+        ix, iy = surf.get_rect().size
+        texid = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D, texid)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, ix, iy, 0, GL_RGBA, GL_UNSIGNED_BYTE, image)
+        return texid
+
+    @classmethod
+    def loadMaterial(cls, filename):
+        contents = {}
+        mtl = None
+        dirname = os.path.dirname(filename)
+
+        for line in open(filename, "r"):
+            if line.startswith('#'): continue
+            values = line.split()
+            if not values: continue
+            if values[0] == 'newmtl':
+                mtl = contents[values[1]] = {}
+            elif mtl is None:
+                raise ValueError("mtl file doesn't start with newmtl stmt")
+            elif values[0] == 'map_Kd':
+                # load the texture referred to by this declaration
+                mtl[values[0]] = values[1]
+                imagefile = os.path.join(dirname, mtl['map_Kd'])
+                mtl['texture_Kd'] = cls.loadTexture(imagefile)
+            else:
+                mtl[values[0]] = list(map(float, values[1:]))
+        return contents
+
     def __init__(self, filename, swapyz=False):
         """Loads a Wavefront OBJ file. """
         self.vertices = []
         self.normals = []
         self.texcoords = []
         self.faces = []
+        self.gl_list = 0
         dirname = os.path.dirname(filename)
 
         material = None
@@ -64,7 +68,7 @@ class OBJ:
             elif values[0] in ('usemtl', 'usemat'):
                 material = values[1]
             elif values[0] == 'mtllib':
-                self.mtl = MTL(os.path.join(dirname, values[1]))
+                self.mtl = self.loadMaterial(os.path.join(dirname, values[1]))
             elif values[0] == 'f':
                 face = []
                 texcoords = []
